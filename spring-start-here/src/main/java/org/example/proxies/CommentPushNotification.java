2283ef8b3078963192064ef89d14dfc0f25b1cfa
senzoo-oh <osj3418@gmail.com>
2024-12-18 01:35:21 +0900

    proxy: 외부와 통신하는 인터페이스와 구현체 생성

---
+++ spring-start-here/src/main/java/org/example/proxies/CommentPushNotification.java
@@ -0,0 +1,15 @@
+package org.example.proxies;
+
+import org.example.model.Comment;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+@Qualifier("PUSH")
+public class CommentPushNotification implements CommentNotificationProxy {
+
+    @Override
+    public void sendComment(Comment comment) {
+        System.out.println("Sending push notification for comment: " + comment.getText());
+    }
+}
