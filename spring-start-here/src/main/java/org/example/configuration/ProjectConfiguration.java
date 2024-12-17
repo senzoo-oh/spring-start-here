8c7c71637ec715441a93ec0a4fb88cd27eb95dc7
senzoo-oh <osj3418@gmail.com>
2024-12-18 01:36:07 +0900

    config: 컨텍스트 관련 구성클래스 생성

---
+++ spring-start-here/src/main/java/org/example/configuration/ProjectConfiguration.java
@@ -0,0 +1,9 @@
+package org.example.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration      // 해당 클래스가 구성클래스임을 나타내는 어노테이션
+@ComponentScan(basePackages = {"org.example.proxies", "org.example.services", "org.example.repositories"})
+public class ProjectConfiguration {
+}
