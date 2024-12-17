c85a80700ef0a3258c557216e548e98fa55f2ffe
senzoo-oh <osj3418@gmail.com>
2024-12-18 01:33:23 +0900

    domain: comment 도메인 생성

---
+++ spring-start-here/src/main/java/org/example/model/Comment.java
@@ -0,0 +1,23 @@
+package org.example.model;
+
+public class Comment {
+
+    private String author;
+    private String text;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
