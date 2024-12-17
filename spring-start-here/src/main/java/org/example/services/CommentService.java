f69c3fca07c217b9665cc535d9641f35bc9f973b
senzoo-oh <osj3418@gmail.com>
2024-12-18 01:42:48 +0900

    스테레오타입 에너테이션: 객체의 책임성을 명시적으로 표시

--- spring-start-here/src/main/java/org/example/services/CommentService.java
+++ spring-start-here/src/main/java/org/example/services/CommentService.java
@@ -4,15 +4,15 @@ import org.example.model.Comment;
 import org.example.proxies.CommentNotificationProxy;
 import org.example.repositories.CommentRepository;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class CommentService {
 
     private final CommentRepository commentRepository;
     private final CommentNotificationProxy commentNotificationProxy;
 
-    // 객체가 생성될 때 생성자의 매개변수로 의존성을 제공함
+    // 객체가 생성될 때 생성자의 매개변수로 의존성을 제공
     public CommentService(
             CommentRepository commentRepository, @Qualifier("PUSH") CommentNotificationProxy commentNotificationProxy
             ) {
