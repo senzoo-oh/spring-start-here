3765330bac576e8b0fb3f1e3bb2264f0f48bffe6
senzoo-oh <osj3418@gmail.com>
2024-12-18 14:06:11 +0900

    지연 인스턴스 생성 방식 확인

--- spring-start-here/src/main/java/org/example/main/Main.java
+++ spring-start-here/src/main/java/org/example/main/Main.java
@@ -1,11 +1,16 @@
 package org.example.main;
 
 import org.example.configuration.ProjectConfiguration;
+import org.example.services.CommentService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
 
         var c = new AnnotationConfigApplicationContext(ProjectConfiguration.class);
+
+        System.out.println("Before retrieving the CommentService");
+        var service = c.getBean(CommentService.class);
+        System.out.println("After retrieving the CommentService");
     }
 }
--- spring-start-here/src/main/java/org/example/services/CommentService.java
+++ spring-start-here/src/main/java/org/example/services/CommentService.java
@@ -1,8 +1,10 @@
 package org.example.services;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 @Service
+@Lazy
 public class CommentService {
 
     public CommentService() {
