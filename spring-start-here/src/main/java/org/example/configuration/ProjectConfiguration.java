7b6b014a729407b576759fbfb9a61f26401f4e19
senzoo-oh <osj3418@gmail.com>
2024-12-18 13:55:11 +0900

    싱글톤 빈, 동일한 참조 확인

--- spring-start-here/src/main/java/org/example/configuration/ProjectConfiguration.java
+++ spring-start-here/src/main/java/org/example/configuration/ProjectConfiguration.java
@@ -1,14 +1,10 @@
 package org.example.configuration;
 
-import org.example.services.CommentService;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration      // 해당 클래스가 구성클래스임을 나타내는 어노테이션
+@ComponentScan(basePackages = {"org.example.services", "org.example.repositories"})
 public class ProjectConfiguration {
 
-    @Bean
-    public CommentService commentService() {
-        return new CommentService();
-    }
 }
