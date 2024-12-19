c72bb10fc31e7fa53d7b1cd561705b07f0a5bd49
senzoo-oh <osj3418@gmail.com>
2024-12-20 02:17:27 +0900

    애스펙트 클래스 빈 등록

--- spring-start-here/src/main/java/org/example/configuration/ProjectConfiguration.java
+++ spring-start-here/src/main/java/org/example/configuration/ProjectConfiguration.java
@@ -1,9 +1,18 @@
 package org.example.configuration;
 
+import org.example.aspects.LoggingAspect;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration      // 해당 클래스가 구성클래스임을 나타내는 어노테이션
-@ComponentScan(basePackages = {"org.example.services", "org.example.repositories", "org.example.processors"})
+@ComponentScan(basePackages = "org.example.services")
+@EnableAspectJAutoProxy     // 스프링 앱에서 애스펙트 메커니즘을 활성화 함
 public class ProjectConfiguration {
+
+    @Bean
+    public LoggingAspect aspect() {
+        return new LoggingAspect();
+    }
 }
