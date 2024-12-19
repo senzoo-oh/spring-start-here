ca6a2295ff54eac21ce34d1f653475fd518ec977
senzoo-oh <osj3418@gmail.com>
2024-12-20 06:35:25 +0900

    @Around 대신 @AfterReturning 어노테이션으로 애스펙트 적용

--- spring-start-here/src/main/java/org/example/aspects/LoggingAspect.java
+++ spring-start-here/src/main/java/org/example/aspects/LoggingAspect.java
@@ -1,6 +1,7 @@
 package org.example.aspects;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.example.model.Comment;
@@ -13,27 +14,12 @@ public class LoggingAspect {
 
     private Logger logger = Logger.getLogger(LoggingAspect.class.getName());
 
-    @Around("@annotation(ToLog)")
-    public Object log(ProceedingJoinPoint joinPoint) throws Throwable {
+    @AfterReturning(value = "@annotation(ToLog)", returning = "returnedValue")
+    public void log(Object returnedValue) {
+        logger.info("Method executed and returned " + returnedValue);
+    }
 
-        /*
-        * joinPoint 매개변수는 가로챈 메서드를 나타냄.
-        * joinPoint.getSignature().getName()을 통해 가로챈 메서드 이름을 가져옴
-        * joinPoint.getArgs()를 통해 가로챈 메서드의 매개변수를 가져옴
-        */
-        String methodName = joinPoint.getSignature().getName();
-        Object [] arguments = joinPoint.getArgs();
-
-        logger.info("Method " + methodName + " with parameters " + Arrays.asList(arguments) + " will execute");
-
-        Comment comment = new Comment();
-        comment.setText("Some other text!");
-        Object[] newArguments = {comment};
-
-        Object returnedByMethod = joinPoint.proceed(newArguments);
-
-        logger.info("Method executed and returned " + returnedByMethod);
-
-        return "FAILED";    // 가로챈 메서드의 반환 값을 반환함
+    public void setLogger(Logger logger) {
+        this.logger = logger;
     }
 }
