aa90474ed087b4bb959151232d753818f77d6712
senzoo-oh <osj3418@gmail.com>
2024-12-22 04:27:10 +0900

    경로 변수를 사용하여 클라이언트에서 서버로 데이터 전송

--- springMVC/src/main/java/com/example/controllers/MainController.java
+++ springMVC/src/main/java/com/example/controllers/MainController.java
@@ -2,16 +2,16 @@ package com.example.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
-    @RequestMapping("/home")
-    public String home(@RequestParam String color, @RequestParam String name,Model page) {
+    @RequestMapping("/home/{color}")
+    public String home(@PathVariable String color, Model page) {
 
-        page.addAttribute("username", name);
+        page.addAttribute("username", "Katy");
         page.addAttribute("color", color);
 
         return "home.html";
