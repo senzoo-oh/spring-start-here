bd82142c3adb0200343d700db588cc89109fee23
senzoo-oh <osj3418@gmail.com>
2024-12-25 23:49:41 +0900

    Rest 컨트롤러 어드바이스를 통한 예외 관리의 책임 분리

---
+++ springMVC/src/main/java/com/example/controllers/PaymentController.java
@@ -0,0 +1,29 @@
+package com.example.controllers;
+
+import com.example.exceptions.NotEnoughMoneyException;
+import com.example.model.ErrorDetails;
+import com.example.model.PaymentDetails;
+import com.example.service.PaymentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    public PaymentController (PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    @PostMapping("/payment")
+    public ResponseEntity<PaymentDetails> makePayment() {
+        PaymentDetails paymentDetails = paymentService.processPayment();
+
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(paymentDetails);
+    }
+}
