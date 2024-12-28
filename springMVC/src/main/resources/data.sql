9dc7f721025b45ac71bf51a454750ede2bd95f44
senzoo-oh <osj3418@gmail.com>
2024-12-29 00:56:28 +0900

    데이터베이스 account 테이블 생성, 테스트 데이터 삽입

--- springMVC/src/main/resources/data.sql
+++ springMVC/src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO account VALUES (NULL, 'Helen Down', 1000);
-INSERT INTO account VALUES (NULL, 'Peter Read', 1000);
+INSERT INTO account VALUES (NULL, 'Jane Down', 1000);
+INSERT INTO account VALUES (NULL, 'John Read', 1000);
--- springMVC/src/main/resources/schema.sql
+++ springMVC/src/main/resources/schema.sql
@@ -1,5 +1,5 @@
-CREATE TABLE IF NOT EXISTS account (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    amount DOUBLE NOT NULL
+create table account (
+     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+     name VARCHAR(50) NOT NULL,
+     amount DOUBLE NOT NULL
 );
