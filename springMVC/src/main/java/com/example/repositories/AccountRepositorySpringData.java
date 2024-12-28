610ee11fc4642bb71ac9908c28929d4ab9cd13aa
senzoo-oh <osj3418@gmail.com>
2024-12-29 00:56:58 +0900

    스프링데이터 프로젝트를 통해 리포지토리 구현

---
+++ springMVC/src/main/java/com/example/repositories/AccountRepositorySpringData.java
@@ -0,0 +1,19 @@
+package com.example.repositories;
+
+import com.example.model.Account;
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface AccountRepositorySpringData extends CrudRepository<Account, Long> {
+
+    @Query("SELECT * FROM account WHERE name = :name")
+    List<Account> findAccountsByName(String name);
+
+    @Modifying
+    @Query("UPDATE account SET amount =:amount WHERE id =:id")
+    void changeAmount(long id, BigDecimal amount);
+}
