38082d3471425299b586254e859c0b0b25808810
senzoo-oh <osj3418@gmail.com>
2024-12-28 22:04:45 +0900

    사용자 정의 데이터소스 구현

--- springMVC/src/main/java/com/example/config/ProjectConfig.java
+++ springMVC/src/main/java/com/example/config/ProjectConfig.java
@@ -1,14 +1,35 @@
 package com.example.config;
 
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import javax.sql.DataSource;
+
 @Configuration
-@EnableFeignClients(basePackages = "com.example.proxy")
 public class ProjectConfig {
+
+    @Value("${custom.datasource.url}")
+    private String datasourceUrl;
+
+    @Value("${custom.datasource.username}")
+    private String datasourceUsername;
+
+    @Value("${custom.datasource.password}")
+    private String datasourcePassword;
+
+
+    @Bean
+    public DataSource dataSource() {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(datasourceUrl);
+        dataSource.setUsername(datasourceUsername);
+        dataSource.setPassword(datasourcePassword);
+        dataSource.setConnectionTimeout(1000);
+        return dataSource;
+    }
 
     @Bean
     public WebClient webClient() {
--- springMVC/src/main/java/com/example/proxy/PaymentsProxy.java
+++ springMVC/src/main/java/com/example/proxy/PaymentsProxy.java
@@ -2,16 +2,9 @@ package com.example.proxy;
 
 import com.example.model.Payment;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-
-import java.util.UUID;
 
 
 @Component
